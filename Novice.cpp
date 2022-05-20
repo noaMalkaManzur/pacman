0b955e6c4440c93c5c00956686555dff0b4de9f6
Noa Malka Manzur <105734534+noaMalkaManzur@users.noreply.github.com>
2022-05-20 09:23:43 +0300

    Delete Novice.cpp

--- Novice.cpp
+++
@@ -1,8 +0,0 @@
-#include "Novice.h"
-
-void Novice::moveGhost(char boardGame[][SIZECOL], int row, int col, ghost& currGhost, const Point& pac, bool keepTrack[][SIZECOL]) {
-	
-	char key = currGhost.MoveRand();
-	currGhost.move(key, boardGame,row,col);//after we get the key move we move the ghosts
-	
-}

ad7de6437053df6e0f8654121e44b8b9d3d842e3
Noa Malka Manzur <105734534+noaMalkaManzur@users.noreply.github.com>
2022-05-20 09:23:52 +0300

    Delete Novice.h

--- Novice.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "ghost.h"
-class Novice :public ghost {
-public:
-	void moveGhost(char boardGame[][SIZECOL], int row, int col, ghost& currGhost, const Point& pac, bool keepTrack[][SIZECOL])override;
-};
-

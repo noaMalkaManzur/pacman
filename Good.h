e23463cd7377d3a91972e9f1c2fb2e104bbc7a34
Noa Malka Manzur <105734534+noaMalkaManzur@users.noreply.github.com>
2022-05-20 09:23:25 +0300

    Delete Good.h

--- Good.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ghost.h"
-#include "QDist.h"
-class Good :public ghost {
-public:
-	void moveGhost(char boardGame[][SIZECOL], int row, int col, ghost& currGhost, const Point& pac, bool keepTrack[][SIZECOL])override;
-};
-
